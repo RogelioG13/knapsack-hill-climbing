82a0d1d406fcd3135301ecd55395f1bdcf7a03e8
RogelioG13 <jimenezrogelio151@gmail.com>
2026-03-14 16:21:25 -0600

    Se agrega la informacion del txt a listas

---
+++ src/leer_instancia.py
@@ -0,0 +1,14 @@
+class Leer_Instancia:
+    def leer_instancia():
+        profits = []
+        weights = []
+        ARCHIVO_INSTANCIA = "data/instancia_mochila.txt"
+        with open(ARCHIVO_INSTANCIA, 'r') as file:
+            cantidad = int(next(file).strip())
+            print(cantidad)
+            for i in range(cantidad):
+                _, profit, weight = map(float, next(file).split())
+                profits.append(profit)
+                weights.append(weight)
+            capacidad = int(next(file).strip())
+        return cantidad, profits, weights, capacidad
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from leer_instancia import Leer_Instancia
+
+cantidad, profits, weights, capacidad = Leer_Instancia.leer_instancia()
+
